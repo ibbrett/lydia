087d5d7805c3c23f717a188e8c0f5e65692a0575
Suva <suva@Bretts-MacBook-Air.local>
2026-01-21 14:39:28 -1000

    Add nested user routes

--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import Link from "next/link";
+
+const users = [
+  { id: 1, name: "Bill" },
+  { id: 2, name: "Henry" },
+  { id: 3, name: "James" },
+];
 
 const Users = () => {
-  return <div>Users</div>;
+  return (
+    <div>
+      <h1>Dashboard Users</h1>
+      <ul className="mt-10">
+        {users.map((user) => {
+          return (
+            <li key={user.id}>
+              <Link href={`/dashboard/users/${user.id}`}>{user.name}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default Users;
