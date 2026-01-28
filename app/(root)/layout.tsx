5b3549637eb27296f494e4f28a0055df0a2a35de
Suva <suva@Bretts-MacBook-Air.local>
2026-01-28 12:53:38 -1000

    Update root layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,12 @@
 import { ReactNode } from "react";
+import type { Metadata } from "next";
+
+const now = new Date();
+
+export const metadata: Metadata = {
+  title: "NFL Teams - " + now.getTime(),
+  description: "NFL Teams | Having fun with Bun!",
+};
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
